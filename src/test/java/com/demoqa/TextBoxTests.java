1f929a0937b9c0f75cc24d17777b2b024ee1e2eb
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2022-11-08 13:59:00 +0300

    add Auth

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -1,16 +1,29 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
 
+    @BeforeAll
+    static void config (){
+        Configuration.baseUrl = "https://russpass.ru/";
+//        Configuration.timeout = 10000; //10 sec
+//        Configuration.browser = "safari";
+    }
+
     @Test
     void fillFormTest(){
         Configuration.holdBrowserOpen = true;
-        open("https://portal.dev01.russpass.dev/");
+        open("login");
+        $("#username").setValue("russpass_test@mail.ru");
+        $("#auth_ok").click();
+        $("#password").setValue("Test1234!");
+        $("#auth_ok").click();
     }
 }
